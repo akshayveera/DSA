ac8f54cd83b0fbb2aed16d08a86a7082f95a27d3
Akshay Veera <akshayveera01@gmail.com>
2024-02-27 23:59:47 +0530

    Feb 27 2024

--- 01 sliding window/01 Max Sum Subarray of size K.cpp
+++ 01 sliding window/01 Max Sum Subarray of size K.cpp
@@ -7,22 +7,65 @@ using namespace std;
 
 long maximumSumSubarray(int K, vector<int> &Arr , int N){        
     
-    long sum = 0;
-    long mx = LONG_MIN;
     
-    for(int i=0; i<N; i++)
-    {
-        sum += Arr[i];
+    long mx = LONG_MIN;
+
+    // // using brute force
+
+    // int i=0;
+
+    // while(i<N-K+1)
+    // {
+    //     long sum = 0;
+    //     int j = i;
+
+    //     while(j<i+K)
+    //     {
+    //         sum += Arr[j];
+    //         j++;
+    //     }
+
+    //     mx = max(mx, sum);
+    //     i++;
+    // }
+
+
+    // using for loop
+    // for(int i=0; i<N; i++)
+    // {
+    //     sum += Arr[i];
         
-        if(i == K-1)
-        {
-            mx = sum;
-        }
-        else if(i > K-1)
-        {
-            sum -= Arr[i-K];
+    //     if(i == K-1)
+    //     {
+    //         mx = sum;
+    //     }
+    //     else if(i > K-1)
+    //     {
+    //         sum -= Arr[i-K];
             
-            mx = max(sum, mx);
+    //         mx = max(sum, mx);
+    //     }
+    // }
+
+
+    // using while loop - two pointer approach
+    long sum = 0;
+
+    int i=0, j=0;
+
+    while(j<N)
+    {
+        sum += Arr[j];
+
+        if(j-i+1 < K)
+        {            
+            j++;
+        }
+        else if(j-i+1 == K){
+            mx = max(mx, sum);
+            sum -= Arr[i];
+            i++;
+            j++;
         }
     }
     
